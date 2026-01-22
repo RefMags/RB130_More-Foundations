c3bb181fba3ac7547aba6f3b768fe6ee19a41065
Ref Mags <fyfymags@gmail.com>
2026-01-22 20:22:36 +0900

    added all implementation methods of todolist class

--- lesson1/09_toDoList_assignment.rb
+++ lesson1/09_toDoList_assignment.rb
@@ -50,7 +50,60 @@ class TodoList
   end
 
   # rest of class needs implementation
+  def <<(task)
+    raise TypeError, "Can only add Todo objects" unless task.instance_of? Todo
+    @todos << task
+  end
+  alias_method :add, :<<
 
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def done?
+    @todos.all? {|task| task.done? }
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def item_at(index)
+    raises ArgumentError unless index
+
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def done!
+    @todos.each_index { |idx| mark_done_at(idx)}
+  end
+
+  def remove_at(index)
+    @todos.delete(item_at(index))
+  end
+
+  def to_s
+    puts "---- #{title} ----"
+    @todos.each do |task|
+      puts task
+    end
+  end
 end
 
 
@@ -67,7 +120,7 @@ list = TodoList.new("Today's Todos")
 list.add(todo1)                 # adds todo1 to end of list, returns list
 list.add(todo2)                 # adds todo2 to end of list, returns list
 list.add(todo3)                 # adds todo3 to end of list, returns list
-list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+# list.add(1)                     # raises TypeError with message "Can only add Todo objects"
 
 # <<
 # same behavior as add
@@ -81,48 +134,42 @@ list.size                       # returns 3
 list.first                      # returns todo1, which is the first item in the list
 
 # last
-list.last                       # returns todo3, which is the last item in the list
-
-#to_a
-list.to_a                      # returns an array of all items in the list
-
-#done?
 list.done?                     # returns true if all todos in the list are done, otherwise false
 
 # ---- Retrieving an item in the list ----
 
 # item_at
-list.item_at                    # raises ArgumentError
+# list.item_at                    # raises ArgumentError
 list.item_at(1)                 # returns 2nd item in list (zero based index)
-list.item_at(100)               # raises IndexError
+# list.item_at(100)               # raises IndexError
 
 # ---- Marking items in the list -----
 
 # mark_done_at
-list.mark_done_at               # raises ArgumentError
-list.mark_done_at(1)            # marks the 2nd item as done
-list.mark_done_at(100)          # raises IndexError
+# list.mark_done_at               # raises ArgumentError
+puts list.mark_done_at(1)            # marks the 2nd item as done
+# list.mark_done_at(100)          # raises IndexError
 
 # mark_undone_at
-list.mark_undone_at             # raises ArgumentError
-list.mark_undone_at(1)          # marks the 2nd item as not done,
-list.mark_undone_at(100)        # raises IndexError
+# list.mark_undone_at             # raises ArgumentError
+# list.mark_undone_at(1)          # marks the 2nd item as not done,
+# list.mark_undone_at(100)        # raises IndexError
 
 # done!
-list.done!                      # marks all items as done
+# list.done!                      # marks all items as done
 
-# ---- Deleting from the list -----
+# # ---- Deleting from the list -----
 
-# shift
-list.shift                      # removes and returns the first item in list
+# # shift
+# list.shift                      # removes and returns the first item in list
 
-# pop
-list.pop                        # removes and returns the last item in list
+# # pop
+# list.pop                        # removes and returns the last item in list
 
 # remove_at
-list.remove_at                  # raises ArgumentError
-list.remove_at(1)               # removes and returns the 2nd item
-list.remove_at(100)             # raises IndexError
+# list.remove_at                  # raises ArgumentError
+# puts list.remove_at(1)               # removes and returns the 2nd item
+# list.remove_at(100)             # raises IndexError
 
 # ---- Outputting the list -----
 
